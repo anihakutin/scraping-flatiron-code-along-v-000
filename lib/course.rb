61b9ec91a251d779b3a9559fb14b856a90691ce3
Heshie Brody <4hbrody@gmail.com>
2018-09-09 05:08:16 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,9 +2,14 @@ class Course
   @@all = [ ]
   attr_accessor :title, :schedule, :description
 
+  def initialize
+      
+  end
+
   def self.all
     @@all
   end
+
   def self.reset_all
     @@all.clear
   end
